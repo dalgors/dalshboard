5a099dd95fe542f06a9c4db4c60820da134c5f2c
solo5star <solo_5@naver.com>
2021-07-21 00:38:46 +0900

    prevent file evaporate when error occurs at jsonfile.write

--- collector/main.py
+++ collector/main.py
@@ -3,8 +3,8 @@ from baekjoon import *
 
 groupId = os.getenv("GROUP_ID")
 cookie = { 
-    "bojautologin" : os.getenv("BOJ_AUTO_LOGIN"),
-    "OnlineJudge" : os.getenv("ONLINE_JUDGE")
+    "bojautologin": os.getenv("BOJ_AUTO_LOGIN"),
+    "OnlineJudge": os.getenv("ONLINE_JUDGE")
 }
 
 recentSubmissions = json.load(open("submissions.json",mode="r",encoding="UTF-8"))
@@ -15,7 +15,11 @@ session.cookies.update(cookie)
 
 try:
     ensureLogin(groupId,session)
-    with open("submissions.json",mode="w",encoding="UTF-8") as jsonfile:
+    with open("submissions.json",mode="r+",encoding="UTF-8") as jsonfile:
+        jsonfile.seek(0)
         jsonfile.write(json.dumps(fetchSubmissionsUntil(groupId,session,recentId)+recentSubmissions,indent=4,ensure_ascii=False))
+        jsonfile.truncate()
+
+    print("성공적으로 크롤링을 완료하였습니다.")
 except CookieExpired as e:
     pass
