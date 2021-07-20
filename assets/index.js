996901081a7203ba80ceffb10567d05d6c74b86e
solo5star <solo_5@naver.com>
2021-07-21 01:04:26 +0900

    assets/index.js: Fix path

--- assets/index.js
+++ assets/index.js
@@ -37,7 +37,7 @@ const app = new Vue({
 		async load() {
 			// load all json files
 			[this.competitions, this.submissions] = await Promise.all(
-				["competitions.json", "collector/submissions.json"].map(async filename => await (await fetch(filename)).json())
+				["competitions.json", "submissions.json"].map(async filename => await (await fetch(filename)).json())
 			);
 
 			// manipulate incomplete fields in json data
