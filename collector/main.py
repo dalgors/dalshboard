1ec6998744960e4af9887bc4b98c7618ffe51298
solo5star <solo_5@naver.com>
2021-07-21 00:41:27 +0900

    collector/main.py: More verbose

--- collector/main.py
+++ collector/main.py
@@ -1,19 +1,18 @@
-import requests,os,json
+import requests,os,json,sys
 from baekjoon import *
 
-groupId = os.getenv("GROUP_ID")
-cookie = { 
-    "bojautologin": os.getenv("BOJ_AUTO_LOGIN"),
-    "OnlineJudge": os.getenv("ONLINE_JUDGE")
-}
-
-recentSubmissions = json.load(open("submissions.json",mode="r",encoding="UTF-8"))
-recentId = recentSubmissions[0]["id"]
-session = requests.Session()
-session.cookies.update(cookie)
-
-
 try:
+    groupId = os.getenv("GROUP_ID")
+    cookie = { 
+        "bojautologin": os.getenv("BOJ_AUTO_LOGIN"),
+        "OnlineJudge": os.getenv("ONLINE_JUDGE")
+    }
+
+    recentSubmissions = json.load(open("submissions.json",mode="r",encoding="UTF-8"))
+    recentId = recentSubmissions[0]["id"]
+    session = requests.Session()
+    session.cookies.update(cookie)
+
     ensureLogin(groupId,session)
     with open("submissions.json",mode="r+",encoding="UTF-8") as jsonfile:
         jsonfile.seek(0)
@@ -22,4 +21,5 @@ try:
 
     print("성공적으로 크롤링을 완료하였습니다.")
 except CookieExpired as e:
-    pass
+    print("프로그램이 예기치 못한 오류로 인해 종료되었습니다")
+    sys.exit(1)
