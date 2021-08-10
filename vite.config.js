5acbdf5f2d79f4221856e85c4a6058eaf18cb5d1
solo5star <solo_5@naver.com>
2021-08-10 18:41:39 +0900

    Fix base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [vue()],
+	base: '/dalshboard/',
 	build: {
 		// GitHub Pages 배포를 위해 docs 폴더에 빌드되도록 지정
 		outDir: 'docs',
